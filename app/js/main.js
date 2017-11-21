e3b428fb2cc55409c0ee352dab50228f3428e7f9
YuryKoropenko <yurykoropenko@gmail.com>
2017-11-21 17:05:15 +0200

    шапка + о проекте

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,24 @@
 $(function() {
-	$('.homeproj__nav img').on('click', function() {
-		var tp = $(this).attr('src');
-		$('homeproj__slider-item img').attr('src', tp);
+	$('.p-aboutSlider').owlCarousel({
+		loop:true,
+		nav:true,
+		items:1
+	});
+	$('.p-aboutSlider2').owlCarousel({
+		loop:true,
+		nav:false,
+		autoplay:true,
+		autoplayTimeout:5000,
+		responsive:{
+			0:{
+				items:2
+			},
+			767:{
+				items:3
+			},
+			992:{
+				items:4
+			}
+		}
 	});
 });
